83a6338e6ef685cd856049cde24d88cb77e3ba51
RizalArfnn <muhammadrifan153@gmail.com>
2017-04-06 15:54:52 +0700

    Edited by Rizal

--- index.php
+++ index.php
@@ -5,13 +5,28 @@
   </head>
   <body>
     <table>
+      <a href="tambah.php">Tambah Data</a>
       <tr>
         <th>No</th>
         <th>Nama</th>
         <th>Jenis Kelamin</th>
       </tr>
         <?php
+        if(!isset($_GET['kirim'])){
+          $kirim = '';
+        }else{
+          $kirim = $_GET['kirim'];
+        }
+
         $konek = mysqli_connect('localhost','root','','merge');
+        if($kirim == 'Kirim!'){
+          $nm = $_GET['nm'];
+          $jk = $_GET['jk'];
+          $sql = "INSERT INTO tbl_nama VALUES('', '$nm', '$jk')";
+          mysqli_query($konek, $sql);
+          header('location:index2.php');
+        }else{
+
         $sql = "SELECT * FROM tbl_nama";
         $query = $konek -> query($sql);
         while($row = $query->fetch_array()){
@@ -21,7 +36,9 @@
           echo '<td>'.$row[2].'</td>';
           echo '</tr>';
         }
+      }
         ?>
     </table>
   </body>
 </html>
+
